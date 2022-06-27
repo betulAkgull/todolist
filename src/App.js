7a93b6f7d42e99b94a9824fb31c8630018777452
betul <betul.akgul@isik.edu.tr>
2022-06-27 22:25:07 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [list,setlist] = React.useState([])
+  const [item,setItem] = React.useState("")
+
+  React.useEffect(()=>{
+    const temp = localStorage.getItem("list")
+    const loadedlist = JSON.parse(temp)
+
+    if(loadedlist){
+      setlist(loadedlist)
+    }
+  },[])
+
+  React.useEffect(()=>{
+    const temp = JSON.stringify(list)
+    localStorage.setItem("list",temp)
+  },[list])
+ 
+ function handleSubmit(e){
+  e.preventDefault()
+
+  const newTodo={
+    id: new Date().getTime(),
+    text: item,
+    completed:false,
+  }
+
+  setlist([...list].concat(newTodo))
+  setItem("")
+ }
+
+ function deleteTodo(id){
+  const updatedlist = [...list].filter((item) => item.id !== id)
+  setlist(updatedlist)
+ }
+
+ function toggleComplete(id){
+  const updatedlist = [...list].map((item)=>{
+    if(item.id === id){
+      item.completed = !item.completed
+    }
+    return item
+  })
+
+  setlist(updatedlist)
+ }
+ 
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setItem(e.target.value)} value = {item}/>
+        <button> Add Todo </button>
+      </form>
+      <p>Todos</p>
+      <ul>
+      {list.map((item)=> 
+      <li key={item.id}>
+        <div>{item.text}</div>
+        <button onClick={() => deleteTodo(item.id)}>Delete</button>
+        <input
+        type = "checkbox"
+        onChange={()=>toggleComplete(item.id)}
+        checked={item.completed} 
+        />
+      </li>)}
+      </ul>
     </div>
   );
 }
